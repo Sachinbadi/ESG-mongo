04f505182ece061cffd344b06f661107c4c80b54
Sachinbadi <106615295+Sachinbadi@users.noreply.github.com>
2024-10-03 18:32:10 +0530

    solved build issue

--- app/analysis/page.tsx
+++ app/analysis/page.tsx
@@ -1,12 +1,21 @@
 'use client';
 
+import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 import PDFAnalysisPage from '../components/PDFAnalysisPage';
 
-export default function AnalysisPage() {
+function AnalysisPageContent() {
   const searchParams = useSearchParams();
   const pdfName = searchParams?.get('pdfName') || '';
   const pdfId = searchParams?.get('pdfId') || '';
 
   return <PDFAnalysisPage pdfName={pdfName} pdfId={pdfId} />;
+}
+
+export default function AnalysisPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <AnalysisPageContent />
+    </Suspense>
+  );
 }
